1275793d3b2b7e3f197efa8ba633890abca9f7a6
bettyp23 <bp23e@fsu.edu>
2025-01-13 15:18:36 -0500

    Starter file

---
+++ BP23EAssgn1.cpp
@@ -0,0 +1,17 @@
+//*****************************************************
+//* Program Name: Suggested Lottery Number Generator  *
+//* Description: Generating lottery tickets based     *
+//               on user input                        *
+//* Date: 01/13/25                                    *
+//* Author: Betty Phipps                              *
+//*****************************************************
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+int main() {
+    //prompt user for information
+
+}
